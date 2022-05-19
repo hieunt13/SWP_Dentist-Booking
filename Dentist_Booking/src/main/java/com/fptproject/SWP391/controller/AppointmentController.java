8d2f4996903e9fa8045955368f5fc4af4bccdedd
hieu <nguyentrunghieu13012002@gmail.com>
2022-05-19 21:57:19 +0700

    update Controller

--- Dentist_Booking/src/main/java/com/fptproject/SWP391/controller/AppointmentController.java
+++ Dentist_Booking/src/main/java/com/fptproject/SWP391/controller/AppointmentController.java
@@ -5,7 +5,6 @@
 package com.fptproject.SWP391.Controller;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -30,19 +29,7 @@ public class AppointmentController extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try ( PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet AppointmentController</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet AppointmentController at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+        System.out.println("t la HIeu");
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
