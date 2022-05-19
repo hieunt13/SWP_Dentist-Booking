d4a477498ab384098ccc9ae5704c19dede6ab661
hieu <nguyentrunghieu13012002@gmail.com>
2022-05-19 22:06:54 +0700

    commit

--- Dentist_Booking/src/main/java/com/fptproject/SWP391/controller/AppointmentController.java
+++ Dentist_Booking/src/main/java/com/fptproject/SWP391/controller/AppointmentController.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-package com.fptproject.SWP391.Controller;
+package com.fptproject.SWP391.controller;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
@@ -29,7 +29,7 @@ public class AppointmentController extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        System.out.println("t la HIeu");
+        System.out.println("t la Hieu asdasdasd");
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
