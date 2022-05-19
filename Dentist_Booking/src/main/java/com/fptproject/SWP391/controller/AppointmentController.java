a9c1909a1f13fffcc8b6740ebdae86a6f4fa8bf4
giavinh <82934507+luugiavinh@users.noreply.github.com>
2022-05-19 22:07:25 +0700

    Update AppointmentController.java

--- Dentist_Booking/src/main/java/com/fptproject/SWP391/controller/AppointmentController.java
+++ Dentist_Booking/src/main/java/com/fptproject/SWP391/controller/AppointmentController.java
@@ -43,6 +43,7 @@ public class AppointmentController extends HttpServlet {
             out.println("</body>");
             out.println("</html>");
         }
+        aa
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
