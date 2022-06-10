9be281756d1b5718b8362ad4b06418aa76efdfda
giavinh <82934507+luugiavinh@users.noreply.github.com>
2022-06-10 19:41:23 +0700

    update promotion image upload admin

--- Dentist_Booking/src/main/java/com/fptproject/SWP391/controller/admin/promotion/AdminUpdatePromotionController.java
+++ Dentist_Booking/src/main/java/com/fptproject/SWP391/controller/admin/promotion/AdminUpdatePromotionController.java
@@ -7,14 +7,21 @@ package com.fptproject.SWP391.controller.admin.promotion;
 import com.fptproject.SWP391.error.PromotionError;
 import com.fptproject.SWP391.manager.admin.AdminPromotionManager;
 import com.fptproject.SWP391.model.Promotion;
+import java.io.File;
 import java.io.IOException;
 import java.sql.Date;
 import java.util.Calendar;
+import java.util.Iterator;
+import java.util.List;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 /**
  *
@@ -32,15 +39,110 @@ public class AdminUpdatePromotionController extends HttpServlet {
             boolean checkError = false;
             Promotion promotion = new Promotion();
             AdminPromotionManager dao = new AdminPromotionManager();
-            String id = request.getParameter("id");
-            String longDescription = request.getParameter("longDescription");
-            String shortDescription = request.getParameter("shortDescription");
-            String newImageName = request.getParameter("image");
-            String currentImage = request.getParameter("currentImage");
-            String promotionName = request.getParameter("promotionName");
-            String expiredDateString = request.getParameter("expiredDate");
-            float discountPercentage = Float.parseFloat(request.getParameter("discountPercentage"));
+            String id = "";//request.getParameter("id");
+            String longDescription = "";//request.getParameter("longDescription");
+            String shortDescription = "";//request.getParameter("shortDescription");
+            String image = "";//request.getParameter("image");
+            String currentImage = "";//request.getParameter("currentImage");
+            String promotionName = "";//request.getParameter("promotionName");
+            String expiredDateString = "";//request.getParameter("expiredDate");
+            float discountPercentage = 0;//Float.parseFloat(request.getParameter("discountPercentage"));
             byte status = 1;
+            
+             // up load image
+            String imgPathTmp = null;
+            File file;
+            int maxFileSize = 5000 * 1024;
+            int maxMemSize = 5000 * 1024;
+            ServletContext context = request.getServletContext();
+            String filePath = context.getInitParameter("file-upload-admin-promotions-folder");//take the path file from web.xml
+            // Verify the content type
+            String contentType = request.getContentType();
+            if ((contentType.indexOf("multipart/form-data") >= 0)) {
+                DiskFileItemFactory factory = new DiskFileItemFactory();
+                // maximum size that will be stored in memory
+                factory.setSizeThreshold(maxMemSize);
+
+                // Location to save data that is larger than maxMemSize.
+                factory.setRepository(new File("D:/Chuyen nganh/SWP391/SWP_Dentist-Booking/Dentist_Booking/src/main/webapp/admin/assets/img/promotions/"));
+
+                // Create a new file upload handler
+                ServletFileUpload upload = new ServletFileUpload(factory);
+
+                // maximum file size to be uploaded.
+                upload.setSizeMax(maxFileSize);
+
+                try {
+                    // Parse the request to get file items.
+                    List fileItems = upload.parseRequest(request);
+
+                    // Process the uploaded file items
+                    Iterator i = fileItems.iterator();
+                    while (i.hasNext()) {
+                        FileItem fi = (FileItem) i.next();
+                        if (fi.isFormField()) {
+                            // get the uploaded file's fields
+                            if (fi.getFieldName().equals("id")) {
+                                id = fi.getString();
+                            }
+                            if (fi.getFieldName().equals("longDescription")) {
+                                longDescription = fi.getString();
+                            }
+                            if (fi.getFieldName().equals("shortDescription")) {
+                                shortDescription = fi.getString();
+                            }
+                            if (fi.getFieldName().equals("promotionName")) {
+                                promotionName = fi.getString();
+                            }
+                            if (fi.getFieldName().equals("expiredDate")) {
+                               expiredDateString = fi.getString();
+                            }
+                            if (fi.getFieldName().equals("discountPercentage")) {
+                               discountPercentage = Float.parseFloat(fi.getString());
+                            }
+                            if (fi.getFieldName().equals("currentImage")) {
+                               currentImage = fi.getString();
+                            }
+
+                        } else {
+                            //process write file to disk
+                            String fieldName = fi.getFieldName();
+                            String fileName = fi.getName();
+                            
+                            // filePath += group + "/" + cmanager.ConvertStringtoName(category) + "/";
+                            boolean isInMemory = fi.isInMemory();
+                            long sizeInBytes = fi.getSize();
+
+                            // Write the file
+                            if(!fileName.isEmpty()){
+                                if (fileName.lastIndexOf("\\") >= 0) {
+                                    file = new File(filePath
+                                            + fileName.substring(fileName.lastIndexOf("\\")));
+                                } else {
+                                    file = new File(filePath
+                                            + fileName.substring(fileName.lastIndexOf("\\") + 1));
+                                }
+                                //get the img path for saving to database
+                                imgPathTmp = file.getAbsolutePath();
+                                fi.write(file);
+                                //create imgpath
+                                String tmp[] = imgPathTmp.split("\\\\");
+                                image = tmp[tmp.length - 4] + "/" + tmp[tmp.length - 3] + "/" + tmp[tmp.length - 2] + "/" + tmp[tmp.length - 1];
+                            }else{
+                                image = currentImage;
+                            }
+                           
+                        }
+
+                    }
+
+                } catch (Exception ex) {
+                    System.out.println(ex);
+                }
+            }
+            
+            
+            // end updload image
             
             if (promotionName.trim().length() < 10 || promotionName.trim().length() > 30) {
                 promotionError.setPromotionNameError("Promotion name must be >= 10 and <=30 characters");
@@ -70,12 +172,6 @@ public class AdminUpdatePromotionController extends HttpServlet {
             }
 
             if (checkError == false) {
-                String image = "assets/img/promotions/";
-                if(newImageName.isEmpty()){
-                    image = currentImage;
-                }else{
-                    image += newImageName;
-                }
                 promotion = new Promotion(id, promotionName.trim(),longDescription.trim(), shortDescription.trim(), image, discountPercentage, expiredDate, status);              
                 if (dao.updatePromotion(promotion)) {
                     url = SUCCESS;
