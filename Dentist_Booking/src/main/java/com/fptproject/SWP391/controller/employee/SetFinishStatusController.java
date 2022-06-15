c630325a37e6cdb0a466c43f0c72ac6eed875c6d
hdangnguyen <hdangprod@gmail.com>
2022-06-15 10:48:01 +0700

    update checkout controller

---
+++ Dentist_Booking/src/main/java/com/fptproject/SWP391/controller/employee/SetFinishStatusController.java
@@ -0,0 +1,87 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package com.fptproject.SWP391.controller.employee;
+
+import com.fptproject.SWP391.manager.employee.EmployeeAppointmentManager;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author dangnguyen
+ */
+@WebServlet(name = "SetFinishStatusController", urlPatterns = {"/SetFinishStatusController"})
+public class SetFinishStatusController extends HttpServlet {
+
+     private static final String SUCCESS = "appointmentEmployee";
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        String url = SUCCESS;
+       try {
+            String ID = request.getParameter("appointmentID");
+            EmployeeAppointmentManager appointmnetDAO = new EmployeeAppointmentManager();
+            if (appointmnetDAO.checkAppointmentStatus(ID) == false) {
+                request.setAttribute("ERROR", "Fail to checkout (This appointment doensn't meet condition)");
+            } else {
+                boolean check = appointmnetDAO.updateFinishAppointment(ID);
+                if (check) {
+                    url = SUCCESS;
+                    request.setAttribute("SUCCESS", "Restrict successfully");
+                }
+            }
+        } catch (Exception e) {
+            log("Error at AdminDeleteDentist Controller: " + e.toString());
+        } finally {
+            request.getRequestDispatcher(url).forward(request, response);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
