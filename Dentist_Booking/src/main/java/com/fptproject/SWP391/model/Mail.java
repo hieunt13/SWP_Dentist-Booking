a2b804b45682cdf93d1e1b260310e8ee030b825a
giavinh <82934507+luugiavinh@users.noreply.github.com>
2022-07-17 23:35:16 +0700

    Update Mail.java

--- Dentist_Booking/src/main/java/com/fptproject/SWP391/model/Mail.java
+++ Dentist_Booking/src/main/java/com/fptproject/SWP391/model/Mail.java
@@ -24,7 +24,7 @@ public class Mail {
     public void send(Appointment appointment, AppointmentDetail[] appoitmentDetail, Customer customer, Dentist dentist, HashMap<String, Service> serviceMap) {
 
         final String user = "vinhlgse161135@fpt.edu.vn";//change accordingly  
-        final String pass = "osxgtbjogpuzxlho";//change accordingly  
+        final String pass = "bwbqayjeosskrwuz";//change accordingly  
 
         //1st step) Get the session object    
         Properties props = new Properties();
